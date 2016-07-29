7a9e1c517bcd9562699cb4ecea84ef68fab95f4b
Zac Heisey <zheisey@gmail.com>
2016-07-29 19:49:41 -0400

    started playing with jQuery flip animation and DOM manipulation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-// Start writing JS & jQuery here
+// Hide Hiragana and Katakana characters
+$('.hiragana').hide();
+$('.katakana').hide();
+
+// Flip/toggle Hiragana characters
+$('.flex-item').click(function() {
+  $('.n-column-n.eigo, .n-column-n.hiragana').fadeToggle('slow', 'linear');
+});
