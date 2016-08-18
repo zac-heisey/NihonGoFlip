3e48d12ea4b9c60ff10cd9d015c7be3d7489ced8
Zac Heisey <zheisey@gmail.com>
2016-08-18 09:37:01 -0400

    test out new approach that flips ALL li elements when nav buttons are clicked

--- js/main.js
+++ js/main.js
@@ -2,10 +2,21 @@
 $('.hiragana').hide();
 $('.katakana').hide();
 
-// Flip/toggle Hiragana characters
+// Flip/toggle ALL Hiragana characters
+$('#hiragana-trigger').on('click', function() {
+  $('li').addClass('animated flipOutY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+    $('li').removeClass('animated flipOutY');
+    $('li').addClass('animated flipInY');
+    $('.eigo, .hiragana').toggle();
+  });
+});
+
+/*
+Flip/toggle INDIVIDUAL Hiragana characters -> Still working on this...
 $('li').on('click', function() {
   $(this).addClass('animated flipOutY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
     $(this).removeClass('animated flipOutY');
     $('.n-column-n.hiragana, .n-column-n.eigo').toggle().addClass('animated flipInY');
   });
 });
+*/
