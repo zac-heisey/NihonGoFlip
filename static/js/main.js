1edaef8c9d79709961e77e3f39ae1ac7497c095f
Zac Heisey <zheisey@gmail.com>
2018-08-25 13:06:10 -0400

    comment out jquery code and work on vanilla js

--- static/js/main.js
+++ static/js/main.js
@@ -1,48 +1,47 @@
 // Hide Hiragana and Katakana characters
-$('.hiragana').hide();
-$('.katakana').hide();
-
-// Flip/toggle ALL Hiragana characters
-$('#hiragana-trigger').on('click', function() {
-  if ($('.katakana').is(':visible')) {
-    $('li').addClass('animated flipInY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-      $('li').removeClass('animated flipInY');
-    });
-      $('.katakana, .hiragana').toggle();
-  } else {
-    $('li').addClass('animated flipInY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-      $('li').removeClass('animated flipInY');
-    });
-      $('.hiragana, .eigo').toggle();
-  }
-});
-
-// Flip/toggle ALL Katakana characters
-$('#katakana-trigger').on('click', function() {
-  if ($('.hiragana').is(':visible')) {
-    $('li').addClass('animated flipInY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-      $('li').removeClass('animated flipInY');
-    });
-      $('.hiragana, .katakana').toggle();
-  } else {
-    $('li').addClass('animated flipInY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-      $('li').removeClass('animated flipInY');
-    });
-      $('.katakana, .eigo').toggle();
-  }
-});
-
-// Flip/toggle individual characters -> Still working on this...
-// $('li').on('click', function() {
+// $('.hiragana').hide();
+// $('.katakana').hide();
+//
+// // Flip/toggle ALL Hiragana characters
+// $('#hiragana-trigger').on('click', function() {
 //   if ($('.katakana').is(':visible')) {
-//     $(this).addClass('animated flipInY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-//       $(this).removeClass('animated flipInY');
+//     $('li').addClass('animated flipInY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+//       $('li').removeClass('animated flipInY');
 //     });
-//       $(this).toggleClass('katakana');
+//       $('.katakana, .hiragana').toggle();
 //   } else {
-//     $(this).addClass('animated flipInY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-//       $(this).removeClass('animated flipInY');
+//     $('li').addClass('animated flipInY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+//       $('li').removeClass('animated flipInY');
 //     });
 //       $('.hiragana, .eigo').toggle();
 //   }
 // });
+//
+// // Flip/toggle ALL Katakana characters
+// $('#katakana-trigger').on('click', function() {
+//   if ($('.hiragana').is(':visible')) {
+//     $('li').addClass('animated flipInY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+//       $('li').removeClass('animated flipInY');
+//     });
+//       $('.hiragana, .katakana').toggle();
+//   } else {
+//     $('li').addClass('animated flipInY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+//       $('li').removeClass('animated flipInY');
+//     });
+//       $('.katakana, .eigo').toggle();
+//   }
+// });
+
+/***
+Flip/toggle individual characters -> Still working on this...
+***/
+
+// Select all <li> flex items (i.e. character squares) -> Returns a NodeList
+var listItems = document.querySelectorAll('li.flex-item');
+
+// Loop through NodeList items -> Add click event listener to individual items -> Add animate.css classes
+for (var i = 0; i < listItems.length; i++) {
+  listItems[i].addEventListener('click', function () {
+    this.classList.add('animated', 'flipInY');
+  }, false);
+}
