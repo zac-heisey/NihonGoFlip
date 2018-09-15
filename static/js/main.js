24eae0551795d6a3e790cdd1a2f5876120ad2cae
Zac Heisey <zheisey@gmail.com>
2018-09-15 18:47:53 -0400

    add active class styles and continue working on li item flip

--- static/js/main.js
+++ static/js/main.js
@@ -36,12 +36,29 @@
 Flip/toggle individual characters -> Still working on this...
 ***/
 
+// Listen for all clicks on the document
+document.addEventListener('click', function() {
+  // Bail if the click is NOT on a page menu link (Hiragana or Katakana)
+  if (!event.target.classList.contains('page-link')) return;
+  // Add .active class
+  event.target.classList.add('active');
+  // Get all page menu links (Hiragana or Katakana)
+  var pageLinks = document.querySelectorAll('.page-link');
+  // Loop through each link
+  for (var i = 0; i < pageLinks.length; i++) {
+    // If the link in the one clicked, skip it
+    if (pageLinks[i] === event.target) continue;
+    // Remove .active class
+    pageLinks[i].classList.remove('active');
+  }
+}, false);
+
 // Select all <li> flex items (i.e. character squares) -> Returns a NodeList
 var listItems = document.querySelectorAll('li.flex-item');
 
 // Loop through NodeList items -> Add click event listener to individual items -> Add animate.css classes
 for (var i = 0; i < listItems.length; i++) {
-  listItems[i].addEventListener('click', function () {
+  listItems[i].addEventListener('click', function() {
     this.classList.add('animated', 'flipInY');
     // Set timeout to remove 'animated' and 'flipInY' classes -> Refactor redundant for() loop below?
     setTimeout(function() {
